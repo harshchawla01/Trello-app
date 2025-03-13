d013784bece0423c2a63561a18dd8c2f4faff885
Harsh Chawla <chawlaharsh890@gmail.com>
2025-03-14 03:04:00 +0530

    Login maintain on reload by onAuthStateChanged in firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,39 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React, { useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./firebase/config";
+import { loginSuccess } from "./redux/reducers/authReducer";
+
+// Components
 import Header from "./components/Layout/Header";
 import Landing from "./components/Layout/Landing";
+import BoardList from "./components/Board/BoardList";
+import PrivateRoute from "./components/Auth/PrivateRoute";
 
 const App = () => {
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        dispatch(
+          loginSuccess({
+            uid: user.uid,
+            displayName: user.displayName,
+            email: user.email,
+            photoURL: user.photoURL,
+          })
+        );
+      }
+      setAuthChecked(true);
+    });
+
+    return () => unsubscribe();
+  }, [dispatch]);
+
   return (
     <Router>
       <div className="min-h-screen flex flex-col">
