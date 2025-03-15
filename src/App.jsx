fb8d00a5c2e9724aec8ac6d1fe29d911e109e8f5
Harsh Chawla <chawlaharsh890@gmail.com>
2025-03-15 21:23:00 +0530

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   Navigate,
 } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./firebase/config";
 import { loginSuccess } from "./redux/reducers/authReducer";
+import { ClipLoader } from "react-spinners";
 
 // Components
 import Header from "./components/Layout/Header";
 import Landing from "./components/Layout/Landing";
 import BoardList from "./components/Board/BoardList";
+import ListContainer from "./components/List/ListContainer";
 import PrivateRoute from "./components/Auth/PrivateRoute";
 
-const App = () => {
+function App() {
+  const dispatch = useDispatch();
+  const [authChecked, setAuthChecked] = useState(false);
+  const { user } = useSelector((state) => state.auth);
+
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
@@ -34,6 +41,14 @@ const App = () => {
     return () => unsubscribe();
   }, [dispatch]);
 
+  if (!authChecked) {
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        <ClipLoader size={50} color="#3B82F6" />
+      </div>
+    );
+  }
+
   return (
     <Router>
       <div className="min-h-screen flex flex-col">
@@ -46,12 +61,13 @@ const App = () => {
             />
             <Route element={<PrivateRoute />}>
               <Route path="/boards" element={<BoardList />} />
+              <Route path="/boards/:boardId" element={<ListContainer />} />
             </Route>
           </Routes>
         </main>
       </div>
     </Router>
   );
-};
+}
 
 export default App;
